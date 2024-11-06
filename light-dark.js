6b56f7720ccd855308c9bd310eff935a25c24a0e
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2024-11-06 12:41:39 +0530

    Initial commit

---
+++ light-dark.js
@@ -0,0 +1,7 @@
+const toggleButton = document.getElementById('mode-toggle');
+const body = document.body;
+
+toggleButton.addEventListener('click', () => {
+      body.classList.toggle('dark-mode');
+      body.classList.toggle('light-mode');
+});
